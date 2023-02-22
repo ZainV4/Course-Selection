00627466ef68e4865bfeb6afd21b73f701a30551
ZainV4 <Zainv4@gmail.com>
2023-02-21 19:18:19 -0500

    update

---
+++ js/main.js
@@ -0,0 +1,23 @@
+/**@author Zain Alsudani*/
+
+import data from './json/courseContent.json' assert { type: 'json'};
+console.log(data);
+
+// variable for the main frame/window
+let mainWebsite = document.getElementsByClassName("page");
+// variable for the title frame/widnow
+let titleWebsite = document.getElementsByClassName("title-page-content");
+
+
+document.getElementById("lol").addEventListener("click", enterWebsite);
+/**
+ * @function enterWebsite
+ * onclick event which will be triggered by clicking the clickMe button.
+ * it changes the display attributes
+ */
+function enterWebsite() {
+    titleWebsite[0].style.display="none"; 
+    mainWebsite[0].style.display="block"; 
+    console.log(mainWebsite);
+    console.log(titleWebsite);
+}
